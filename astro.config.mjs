78667890a10af0595354b246997b9bf23a18dbfc
Diego Quipuzco <demential2003@gmail.com>
2025-08-07 10:01:48 -0500

    workflow & pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dMeenta.github.io',
+  base: 'Diego-quipuzco',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [preact()]
 });
